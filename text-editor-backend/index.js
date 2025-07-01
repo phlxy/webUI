ccc5a46c7b4abb5e634b376adb42d3bfe098481c
puttipong <puttipong.le0695@gmil.com>
2025-07-01 14:19:27 +0700

    backend:index.js update json limit 50mb

--- text-editor-backend/index.js
+++ text-editor-backend/index.js
@@ -6,7 +6,8 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // API สำหรับดึงข้อมูล summary_json จากตาราง DOC
 app.get('/api/doc', async (req, res) => {
